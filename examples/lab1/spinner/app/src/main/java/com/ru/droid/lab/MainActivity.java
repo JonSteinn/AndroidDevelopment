150ef8f6839267d2c6a40f44d46b4518cc821ac0
JonSteinn <jonsteinn@gmail.com>
2017-10-06 10:59:11 +0000

    spinner example

---
+++ examples/lab1/spinner/app/src/main/java/com/ru/droid/lab/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ru.droid.lab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String entries[] = new String[]{"item 1", "item 2", "item 3"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, entries);
+        Spinner spinner = (Spinner)findViewById(R.id.my_spinner);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String msg = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(MainActivity.this, R.string.none, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+}
