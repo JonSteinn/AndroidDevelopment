9493490b8c112bbe318d50a6bc244287652f7225
JonSteinn <jonsteinn@gmail.com>
2017-10-06 08:30:23 +0000

    lab1 - event example

---
+++ examples/lab1/event/app/src/main/java/com/ru/droid/lab/MainActivity.java
@@ -0,0 +1,28 @@
+package com.ru.droid.lab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMethod(View view) {
+        Button btn = (Button)findViewById(R.id.my_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+                Log.d("Event", "Click");
+            }
+        });
+    }
+}
