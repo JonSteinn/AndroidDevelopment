013f8a9c8a3296ead2a370a1dde989a435f7a16f
JonSteinn <jonsteinn@gmail.com>
2017-10-21 20:36:48 +0000

    start act for res

---
+++ examples/lab3/activityforresult/app/src/main/java/com/ru/droid/lab/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ru.droid.lab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_ID = 1111;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, REQ_ID);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQ_ID) {
+            if (resultCode == RESULT_OK) {
+                String val = data.getStringExtra("MSG");
+                ((Button) findViewById(R.id.btn)).setText(val);
+            } else {
+                Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ examples/lab3/activityforresult/app/src/main/java/com/ru/droid/lab/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.ru.droid.lab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void sendBackResults(View view) {
+        Intent intent = new Intent();
+        EditText msg = (EditText)findViewById(R.id.msg_id);
+        intent.putExtra("MSG", msg.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
