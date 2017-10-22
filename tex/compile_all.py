f6b4e547e41edc99db0527b528a1c4d8af1bfbd2
JonSteinn <jonsteinn@gmail.com>
2017-10-22 13:48:56 +0000

    compile all and move to pdf folder in root py script

---
+++ tex/compile_all.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+from shutil import copyfile
+
+# re-visit ef > 9
+s = "\\input{{settings.tex}}\r\n\\begin{{document}}\r\n\input{{lab0{0}/lab0{0}.tex}}\r\n\\end{{document}}"
+labs_count = 4
+
+
+def create_main(lab):
+    fd = open("main.tex", "w")
+    fd.write(s.format(lab))
+    fd.close()
+
+
+def run_py_script():
+    pth = os.path.abspath(".")
+    cmd = "python compile.py"
+    process = subprocess.Popen(args=cmd, shell=False, stdout=subprocess.PIPE, cwd=pth)
+    process.wait()
+
+
+def move_pdf(lab):
+    if os.path.isfile("main.pdf"):
+        copyfile("main.pdf", "../pdf/lab0{0}.pdf".format(lab))
+        os.remove("main.pdf")
+
+
+for i in range(labs_count):
+    create_main(i)
+    run_py_script()
+    move_pdf(i)
